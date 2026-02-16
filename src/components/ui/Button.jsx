70c8125c09ad9e795ee9d9289b12380da6d7c319
nadiry <usmannadiry75@gmail.com>
2026-02-16 10:28:22 +0500

    btn styled changes

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -4,43 +4,43 @@ const StyledButton = styled.button`
   padding: 10px 30px;
   border-radius: 6px;
   font-weight: 500;
-  cursor: ${({ disabled }) => (disabled ? "not-allowed" : "pointer")};
   border: none;
+  color: white;
   transition: all 0.2s ease;
 
-  background-color: ${({ variant }) =>
-    variant === "danger"
-      ? "#dc2626"
-      : variant === "success"
-        ? "#16a34a"
-        : variant === "warning"
-          ? "#eab308"
-          : "#4f46e5"};
+  cursor: ${({ disabled }) => (disabled ? "not-allowed" : "pointer")};
 
-  color: white;
+  background-color: ${({ disabled, variant }) => {
+    if (disabled) return "#dadada";
+
+    switch (variant) {
+      case "danger":
+        return "#dc2626";
+      case "success":
+        return "#16a34a";
+      case "warning":
+        return "#eab308";
+      default:
+        return "#4f46e5"; // primary
+    }
+  }};
 
   &:hover {
-    opacity: 0.85;
+    opacity: ${({ disabled }) => (disabled ? 1 : 0.85)};
   }
 
   &:active {
-    transform: scale(0.97);
+    transform: ${({ disabled }) => (disabled ? "none" : "scale(0.97)")};
   }
 
   @media (min-width: 768px) {
-    & {
-      padding: 15px 50px;
-    }
-  }
-
-  @media (min-width: 900px) {
     padding: 15px 50px;
   }
 `;
 
 const Button = ({ children, variant = "primary", disabled, ...props }) => {
   return (
-    <StyledButton variant={variant} {...props} disabled={disabled}>
+    <StyledButton variant={variant} disabled={disabled} {...props}>
       {children}
     </StyledButton>
   );
